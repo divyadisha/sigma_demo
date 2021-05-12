6849a86b9a736032b66d9b42b85aff32d06bdef7
divyadisha <divyasandeepnihu@gmail.com>
2021-05-12 18:25:55 +0530

    read from s3

---
+++ sigma_demo.py
@@ -0,0 +1,39 @@
+import boto3
+ddb = boto3.client("dynamodb")
+import pandas as pd
+s3 = boto3.client("s3")
+
+def read_file(event, context):
+    # print("Reading from s3...")
+    try:
+        data = s3.get_object(
+            Bucket="awssigmabocket",
+            Key="testdata.csv"
+        )
+        books_df = pd.read_csv(data.get("Body"))
+        print(books_df)
+        
+        for i in range(len(books_df)):
+            try:
+                dy_db = ddb.put_item(
+                    TableName="emptable",
+                    Item={
+                        'id': {
+                            'N': str(books_df['id'][i])
+                        },
+                        'name': {
+                            'S': books_df['name'][i]
+                        }
+                    }
+                )
+                print(dy_db,"@@@")
+            except BaseException as e:
+                print(e)
+                raise(e)
+            
+    except BaseException as e:
+        print(e)
+        raise(e)
+    
+    return {"message": "Successfully read file from S3 and inserted to DDB"}
+
